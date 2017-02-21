0d4decef937ae2331e002f81b1ab80a033d513c8
Alicia McNett <alicia.mcnett@gmail.com>
2017-02-21 12:59:06 -0500

    for today's in-class
    Java project files

---
+++ ICE5/src/OverflowExercise.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public final class OverflowExercise
+{
+	public static void main(String[] args)
+	{
+		short acctBal = Short.MAX_VALUE - 3000; // Take note of the data type for acctBal!
+		
+		String input = "0";
+		Scanner scanner = new Scanner(System.in);
+		
+		while(true)
+		{
+			System.out.println("You currently have an account balance of $" + acctBal + ".");
+			System.out.print("Deposit how much? ");
+			
+			input = scanner.nextLine().trim();
+			
+			try
+			{
+				acctBal += Integer.parseInt(input);
+			}
+			catch(NumberFormatException nfe)
+			{
+				if(input.toLowerCase().equals("quit"))
+				{
+					break;
+				}
+				else
+				{
+					System.out.println("Sorry, I don't recognize that number.");
+				}
+			}
+		}
+		
+		System.out.println("Thank you, and goodbye.");
+		scanner.close();
+	}	
+}
